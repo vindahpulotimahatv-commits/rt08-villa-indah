56bc3ff78aa009824e4ed785a1a6ab5039d4b383
vindahpulotimahatv-commits <vindahpulotimahatv@gmail.com>
2026-09-21 10:29:01 +0700

    Add files via upload

---
+++ warga-data.js
@@ -0,0 +1,700 @@
+/* Data induk warga RT 08 / RW 021 - Blok, Nomor Rumah, Nama Kepala Keluarga.
+   Diimpor dari DATA_INDUK_RT_008_RW_021.xlsx. Untuk update data warga baru/pindah,
+   admin cukup edit array WARGA_DATA di bawah ini lalu upload ulang file ini ke GitHub. */
+const WARGA_DATA = [
+  {
+    "blok": "E2",
+    "no": "1",
+    "nama": "BIMBA"
+  },
+  {
+    "blok": "E2",
+    "no": "2",
+    "nama": "AGUS ARIYANTO"
+  },
+  {
+    "blok": "E2",
+    "no": "3",
+    "nama": "RANTAWINATA"
+  },
+  {
+    "blok": "E2",
+    "no": "3A",
+    "nama": "HERMAN FAUZI"
+  },
+  {
+    "blok": "E2",
+    "no": "5",
+    "nama": "DIDIK SETIJADI"
+  },
+  {
+    "blok": "E2",
+    "no": "6",
+    "nama": "ALIF SETIAWAN"
+  },
+  {
+    "blok": "E2",
+    "no": "7",
+    "nama": "BASUKI WIDODO"
+  },
+  {
+    "blok": "E2",
+    "no": "8",
+    "nama": "JOKO SUSILO"
+  },
+  {
+    "blok": "E2",
+    "no": "9",
+    "nama": "MUSLIKIN"
+  },
+  {
+    "blok": "E2",
+    "no": "10",
+    "nama": "SOFIAN KZ"
+  },
+  {
+    "blok": "E2",
+    "no": "11",
+    "nama": "ARNOLD ARIFIN"
+  },
+  {
+    "blok": "E2",
+    "no": "12",
+    "nama": "BUDI PURNOMO"
+  },
+  {
+    "blok": "E2",
+    "no": "12A",
+    "nama": "KARTO"
+  },
+  {
+    "blok": "E2",
+    "no": "14",
+    "nama": "RIO ISKANDAR"
+  },
+  {
+    "blok": "E2",
+    "no": "15",
+    "nama": "AHMAD NGUDI"
+  },
+  {
+    "blok": "E2",
+    "no": "16",
+    "nama": "NURHAYATI"
+  },
+  {
+    "blok": "E2",
+    "no": "17",
+    "nama": "DALIH KUSUMA WARDANA"
+  },
+  {
+    "blok": "E2",
+    "no": "18",
+    "nama": "IDHAR TAUFIK MAULAMA"
+  },
+  {
+    "blok": "E2",
+    "no": "19",
+    "nama": "GITA RIFAIDA"
+  },
+  {
+    "blok": "E2",
+    "no": "20",
+    "nama": "SETYA BUDIYONO"
+  },
+  {
+    "blok": "E2",
+    "no": "21",
+    "nama": "ZULHAM"
+  },
+  {
+    "blok": "E2",
+    "no": "22",
+    "nama": "DIHANTORO"
+  },
+  {
+    "blok": "E2",
+    "no": "24",
+    "nama": "DJITRO LAGA RAE"
+  },
+  {
+    "blok": "E2",
+    "no": "25",
+    "nama": "ALFONSUS ROLI EKOATMOJO"
+  },
+  {
+    "blok": "E2",
+    "no": "26",
+    "nama": "KAMIDI"
+  },
+  {
+    "blok": "E2",
+    "no": "27",
+    "nama": "ARI MARGO PRASETYO"
+  },
+  {
+    "blok": "E2",
+    "no": "28",
+    "nama": "SUPRAPTO"
+  },
+  {
+    "blok": "E2",
+    "no": "29",
+    "nama": "ANDI SETIANDI"
+  },
+  {
+    "blok": "E2",
+    "no": "30",
+    "nama": "MUHAMMAD ANSHORI"
+  },
+  {
+    "blok": "E2",
+    "no": "31",
+    "nama": "DADI PURMANTO"
+  },
+  {
+    "blok": "E2",
+    "no": "32",
+    "nama": "DALIH KUSUMA WARDANA"
+  },
+  {
+    "blok": "E2",
+    "no": "33",
+    "nama": "DRAJAT"
+  },
+  {
+    "blok": "E2",
+    "no": "33A",
+    "nama": "MUZIAH PUTRI PERDANA"
+  },
+  {
+    "blok": "E2",
+    "no": "35",
+    "nama": "RIVAN YULIANTO"
+  },
+  {
+    "blok": "E2",
+    "no": "36",
+    "nama": "ABD HAFIZ"
+  },
+  {
+    "blok": "E2",
+    "no": "37",
+    "nama": "AGUS SUSANTO"
+  },
+  {
+    "blok": "E2",
+    "no": "38",
+    "nama": "WAHYU SUPRIYONO"
+  },
+  {
+    "blok": "E2",
+    "no": "39",
+    "nama": "MUHAMMAD IQBAL FADLI"
+  },
+  {
+    "blok": "E2",
+    "no": "40",
+    "nama": "MUHAMAD AJRUL KAMSIN"
+  },
+  {
+    "blok": "E2",
+    "no": "41",
+    "nama": "WISNU PRIAMBODO"
+  },
+  {
+    "blok": "E2",
+    "no": "42",
+    "nama": "BAGOES RIYADI KURNIAWAN"
+  },
+  {
+    "blok": "E2",
+    "no": "43",
+    "nama": "LILIK"
+  },
+  {
+    "blok": "E2",
+    "no": "44",
+    "nama": "ADIH KURNIAWAN"
+  },
+  {
+    "blok": "E2",
+    "no": "45",
+    "nama": "SUPRIYATNO"
+  },
+  {
+    "blok": "E2",
+    "no": "46",
+    "nama": "ABDURRACHMAN ABDULLAH"
+  },
+  {
+    "blok": "E2",
+    "no": "47",
+    "nama": "RAYNAL IHSANUL AZHAR"
+  },
+  {
+    "blok": "E2",
+    "no": "48",
+    "nama": "ARIO WIJAYA"
+  },
+  {
+    "blok": "E5",
+    "no": "1",
+    "nama": "IKSAN"
+  },
+  {
+    "blok": "E5",
+    "no": "2",
+    "nama": "BAYU RAMADHANI"
+  },
+  {
+    "blok": "E5",
+    "no": "3",
+    "nama": "HERMAN"
+  },
+  {
+    "blok": "E5",
+    "no": "3A",
+    "nama": "LUKMAN ZAKY ADI NUGROHO"
+  },
+  {
+    "blok": "E5",
+    "no": "5",
+    "nama": "YUSUF HIDAYAT"
+  },
+  {
+    "blok": "E5",
+    "no": "6",
+    "nama": "ARDIANSYAH ZUNAIDI"
+  },
+  {
+    "blok": "E5",
+    "no": "7",
+    "nama": "KURNIA SUBHAN"
+  },
+  {
+    "blok": "E5",
+    "no": "8",
+    "nama": "HENDYANTO"
+  },
+  {
+    "blok": "E5",
+    "no": "9",
+    "nama": "MARSUDI"
+  },
+  {
+    "blok": "E5",
+    "no": "10",
+    "nama": "MOHAMMAD JAUHARI"
+  },
+  {
+    "blok": "E5",
+    "no": "11",
+    "nama": "DARMAN SUSILO"
+  },
+  {
+    "blok": "E5",
+    "no": "12",
+    "nama": "BAHREISY ARI MURFA"
+  },
+  {
+    "blok": "E5",
+    "no": "12A",
+    "nama": "HANIYASIH"
+  },
+  {
+    "blok": "E5",
+    "no": "14",
+    "nama": "M KHASAN BASRI"
+  },
+  {
+    "blok": "E5",
+    "no": "15",
+    "nama": "JOKO PRIYONO"
+  },
+  {
+    "blok": "E5",
+    "no": "16",
+    "nama": "MOCH FAOZI"
+  },
+  {
+    "blok": "E5",
+    "no": "17",
+    "nama": "DANANG ISDIANTO"
+  },
+  {
+    "blok": "E5",
+    "no": "18",
+    "nama": "RATIH NURKUMALASARI"
+  },
+  {
+    "blok": "E5",
+    "no": "19",
+    "nama": "RONAL TAMPUBOLON"
+  },
+  {
+    "blok": "E5",
+    "no": "20",
+    "nama": "INDAH WINDYA FURRI, SS"
+  },
+  {
+    "blok": "E6",
+    "no": "1",
+    "nama": "TAKDIM SAEFUDIN"
+  },
+  {
+    "blok": "E6",
+    "no": "2",
+    "nama": "MESLA MANALU"
+  },
+  {
+    "blok": "E6",
+    "no": "3",
+    "nama": "MUHAMMAD ANDRE"
+  },
+  {
+    "blok": "E6",
+    "no": "3A",
+    "nama": "MIFTAHUL ULUM"
+  },
+  {
+    "blok": "E6",
+    "no": "5",
+    "nama": "YOSICO LOUPATTY"
+  },
+  {
+    "blok": "E6",
+    "no": "6",
+    "nama": "MUHAMMAD DENIS YUSNANDAR"
+  },
+  {
+    "blok": "E6",
+    "no": "7",
+    "nama": "RIYAN WIBIKSONO"
+  },
+  {
+    "blok": "E6",
+    "no": "8",
+    "nama": "ANDI SETIANDI"
+  },
+  {
+    "blok": "E6",
+    "no": "9",
+    "nama": "AGUNG PRASETIYO WIBOWO"
+  },
+  {
+    "blok": "E6",
+    "no": "10",
+    "nama": "DEDI SANJAYA"
+  },
+  {
+    "blok": "E6",
+    "no": "11",
+    "nama": "ROKHIMAN"
+  },
+  {
+    "blok": "E6",
+    "no": "12",
+    "nama": "SYARIEF H. M."
+  },
+  {
+    "blok": "E6",
+    "no": "12A",
+    "nama": "ANDI MAPPAPOLEONRO"
+  },
+  {
+    "blok": "E6",
+    "no": "14",
+    "nama": "SUDARMAWAN"
+  },
+  {
+    "blok": "E6",
+    "no": "15",
+    "nama": "ADITYA GUMELAR EKAPUTRA"
+  },
+  {
+    "blok": "E6",
+    "no": "16",
+    "nama": "PONIDIN"
+  },
+  {
+    "blok": "E6",
+    "no": "17",
+    "nama": "DENI IRLANTO"
+  },
+  {
+    "blok": "E6",
+    "no": "18",
+    "nama": "MUKHALIM"
+  },
+  {
+    "blok": "E6",
+    "no": "19",
+    "nama": "UNIK LESTIYATI"
+  },
+  {
+    "blok": "E6",
+    "no": "20",
+    "nama": "AGUS SUKANDA"
+  },
+  {
+    "blok": "E6",
+    "no": "21",
+    "nama": "PONIJO"
+  },
+  {
+    "blok": "E6",
+    "no": "22",
+    "nama": "MUHAMMAD EQUE FAJARULLAH"
+  },
+  {
+    "blok": "E6",
+    "no": "23",
+    "nama": "HASIHOLAN HASUGIAN"
+  },
+  {
+    "blok": "E6",
+    "no": "24",
+    "nama": "SYAHRIL HOLIDY"
+  },
+  {
+    "blok": "E7",
+    "no": "1",
+    "nama": "JEFRI"
+  },
+  {
+    "blok": "E7",
+    "no": "2",
+    "nama": "IRFANI"
+  },
+  {
+    "blok": "E7",
+    "no": "3",
+    "nama": "RIZKI ARIJANSYAH"
+  },
+  {
+    "blok": "E7",
+    "no": "3A",
+    "nama": "FIRMAN MAULANA ANGGREAWAN"
+  },
+  {
+    "blok": "E7",
+    "no": "5",
+    "nama": "MURDIYANTO"
+  },
+  {
+    "blok": "E7",
+    "no": "6",
+    "nama": "RIZKY KUSUMA PUTRA"
+  },
+  {
+    "blok": "E7",
+    "no": "7",
+    "nama": "MARKUS EKO TRI WIDAGDO"
+  },
+  {
+    "blok": "E7",
+    "no": "8",
+    "nama": "GRACELINA RIBKAH PUIDE"
+  },
+  {
+    "blok": "E7",
+    "no": "9",
+    "nama": "AGUNG GRIYADI"
+  },
+  {
+    "blok": "E7",
+    "no": "10",
+    "nama": "FIQI MAULANA"
+  },
+  {
+    "blok": "E7",
+    "no": "11",
+    "nama": "MUHIDIN ZAELANI"
+  },
+  {
+    "blok": "E7",
+    "no": "12",
+    "nama": "M.SOLIKHUDIN"
+  },
+  {
+    "blok": "E7",
+    "no": "12A",
+    "nama": "EKO AGUS SISWANTO"
+  },
+  {
+    "blok": "E7",
+    "no": "14",
+    "nama": "GUDANG M. ULUM"
+  },
+  {
+    "blok": "E7",
+    "no": "15",
+    "nama": "DIPO PURNOMO"
+  },
+  {
+    "blok": "E7",
+    "no": "16",
+    "nama": "SIM YENNI"
+  },
+  {
+    "blok": "E7",
+    "no": "17",
+    "nama": "KUSENO"
+  },
+  {
+    "blok": "E7",
+    "no": "18",
+    "nama": "SUPANGAT"
+  },
+  {
+    "blok": "E7",
+    "no": "19",
+    "nama": "SILVESTER PETRUS HARTO"
+  },
+  {
+    "blok": "E7",
+    "no": "20",
+    "nama": "TOGU HASIHOLAN SINAGA"
+  },
+  {
+    "blok": "E7",
+    "no": "21",
+    "nama": "DANIEL JANUARTO"
+  },
+  {
+    "blok": "E7",
+    "no": "22",
+    "nama": "MIFTAHUL KHOERI"
+  },
+  {
+    "blok": "E7",
+    "no": "23",
+    "nama": "MUHLISIN"
+  },
+  {
+    "blok": "E7",
+    "no": "24",
+    "nama": "AGUS FIRMANSYAH"
+  },
+  {
+    "blok": "E8",
+    "no": "1",
+    "nama": "SUGENG RIYADI"
+  },
+  {
+    "blok": "E8",
+    "no": "2",
+    "nama": "SLAMET RIYADI"
+  },
+  {
+    "blok": "E8",
+    "no": "3",
+    "nama": "IRFAN MAULANA"
+  },
+  {
+    "blok": "E8",
+    "no": "3A",
+    "nama": "YULMARIZON"
+  },
+  {
+    "blok": "E8",
+    "no": "5",
+    "nama": "WASITO"
+  },
+  {
+    "blok": "E8",
+    "no": "6",
+    "nama": "SUPRIYATONO"
+  },
+  {
+    "blok": "E8",
+    "no": "7",
+    "nama": "MITRIYANI"
+  },
+  {
+    "blok": "E8",
+    "no": "8",
+    "nama": "AZRAN ARDIANSYAH"
+  },
+  {
+    "blok": "E8",
+    "no": "9",
+    "nama": "ABDULLAH"
+  },
+  {
+    "blok": "E8",
+    "no": "10",
+    "nama": "JONSON SIALLAGAN"
+  },
+  {
+    "blok": "E8",
+    "no": "11",
+    "nama": "KRISTINA MARNI MANURUNG"
+  },
+  {
+    "blok": "E8",
+    "no": "12",
+    "nama": "HENDRIX MOCHAMMAD MIRZA"
+  },
+  {
+    "blok": "E8",
+    "no": "12A",
+    "nama": "YURIANA MARUHAWA"
+  },
+  {
+    "blok": "E8",
+    "no": "14",
+    "nama": "JOKO ARIANTO"
+  },
+  {
+    "blok": "E8",
+    "no": "15",
+    "nama": "KIKI NOVIAR"
+  },
+  {
+    "blok": "E8",
+    "no": "16",
+    "nama": "WINDU IMAN SANTOSO"
+  },
+  {
+    "blok": "E8",
+    "no": "17",
+    "nama": "DANIS PUTRA KARUNIAWAN"
+  },
+  {
+    "blok": "E8",
+    "no": "18",
+    "nama": "ACHMAD MAULANA"
+  },
+  {
+    "blok": "E8",
+    "no": "19",
+    "nama": "BRANGGA"
+  },
+  {
+    "blok": "E8",
+    "no": "20",
+    "nama": "AZIZ BAEHAKI"
+  },
+  {
+    "blok": "E8",
+    "no": "21",
+    "nama": "TEGUH JUMADI"
+  },
+  {
+    "blok": "E8",
+    "no": "22",
+    "nama": "MUHAIMIN NUR ROSYID"
+  },
+  {
+    "blok": "E8",
+    "no": "23",
+    "nama": "KOSONG"
+  },
+  {
+    "blok": "E8",
+    "no": "24",
+    "nama": "JOHARI PERNANDO RIO NABABAN"
+  }
+];
